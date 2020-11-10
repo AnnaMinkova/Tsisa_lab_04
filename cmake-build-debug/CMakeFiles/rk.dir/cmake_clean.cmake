7eedf8634ad310db9b82d50845d4bfff71812c33
AnnaMinkova <anaminkova@yandex.ru>
2020-11-10 13:35:01 +0300

    main.cpp

---
+++ cmake-build-debug/CMakeFiles/rk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rk.dir/main.cpp.o"
+  "rk"
+  "rk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
